bb7a485d4a79c083211d91e5bcabba9d174b70ab
Alex <69348601+Alex-935@users.noreply.github.com>
2025-09-09 12:31:48 +0100

    Breakpoint testing

--- ___helloWorld.java
+++ ___helloWorld.java
@@ -1,7 +1,16 @@
 public class ___helloWorld {
 
     public static void main(String[] args) {
-        
-        System.out.println("Hello World");
+
+        String greeting = "Hello World";
+        System.out.println(greeting);
+
+        int number = 5;
+        int doubled = doubleNumber(number);
+        System.out.println("The doubled number is " + doubled);
+    }
+
+    static int doubleNumber(int n) {
+        return n * 2;
     }
 }
